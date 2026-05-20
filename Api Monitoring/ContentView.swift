90e8b5de786029e7ab1038a0c021411cf26fdba4
Kevin Carmona <kcarmona547@gmail.com>
2026-05-19 22:29:04 -0600

    Add items to the left sidebar

--- Api Monitoring/ContentView.swift
+++ Api Monitoring/ContentView.swift
@@ -8,14 +8,63 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        NavigationSplitView( sidebar: {
+            VStack(alignment: .leading, spacing: 16) {
+                HStack {
+                    Text("PROJECTS")
+                        .font(.title3)
+                        .fontWeight(.bold)
+                        .foregroundColor(.gray)
+                    Spacer()
+                }
+                .padding(.leading, 12)
+                NavigationViewItem(
+                    icon: "display",
+                    count: 15,
+                    title: "All monitors",
+                    imageForeground: .white
+                )
+                
+                Divider()
+                
+                HStack {
+                    Text("STATUS")
+                        .font(.title3)
+                        .fontWeight(.bold)
+                        .foregroundColor(.gray)
+                    Spacer()
+                }
+                .padding(.leading, 12)
+                NavigationViewItem(
+                    icon: "circle",
+                    count: 3,
+                    title: "Up",
+                    imageForeground: .green
+                )
+                NavigationViewItem(
+                    icon: "exclamationmark.triangle",
+                    count: 10,
+                    title: "Warning",
+                    imageForeground: .yellow
+                )
+                NavigationViewItem(
+                    icon: "x.circle",
+                    count: 2,
+                    title: "Down",
+                    imageForeground: .red
+                )
+                
+                Spacer()
+            }
+            .padding(.horizontal, 8)
+            .padding(.top, 16)
+            .navigationSplitViewColumnWidth(min: 260, ideal: 260, max: 260)
+        },
+ detail: {
+            
+        })
     }
 }
 
